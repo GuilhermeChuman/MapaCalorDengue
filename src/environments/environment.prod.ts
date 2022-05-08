e1365625f602c2952071f5dec7fa777e9f48ca23
GuilhermeChuman <guilhermectroncon@gmail.com>
2022-05-08 15:15:59 -0300

    limites ajustados

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  api_url: 'http://localhost:8000/api/',
+
+  //casos
+  casos_mes_geoJson: 'casosNoMes/GeoJson',
+  casos_by_mes: 'casosByMes',
+  casos_by_idade: 'casosByIdade',
+  casos_by_bairro: 'casosByBairro',
+  casos_mes: 'casosNoMes'
 };
