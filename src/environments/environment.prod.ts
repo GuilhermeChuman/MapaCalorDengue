49dbb17a54405ef4c05c5302c43911af3c136350
GuilhermeChuman <guilhermectroncon@gmail.com>
2022-05-08 17:55:04 -0300

    api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api_url: 'http://localhost:8000/api/',
+  api_url: 'https://mapadengue.000webhostapp.com/api/',
 
   //casos
   casos_mes_geoJson: 'casosNoMes/GeoJson',
